5cf0f819c21213d51c85f453057a6cf97523fede
Liran Glikman <liranglikm@gmail.com>
2018-12-29 17:42:38 +0200

    Fixed some styles and set isLoggedIn to false on site startup

--- client/src/app/core/auth.service.ts
+++ client/src/app/core/auth.service.ts
@@ -7,7 +7,7 @@ import { tap, delay } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn = true;
+  isLoggedIn = false;
 
   constructor(private router: Router) { }
 
