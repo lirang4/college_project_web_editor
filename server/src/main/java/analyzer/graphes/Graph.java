f6a0515fb2fab1d0965b5b61e88ed2bf1eb23541
Tom <assatom2@gmail.com>
2019-06-07 13:22:55 +0300

    InitializeParametersNumber

--- server/src/main/java/analyzer/graphes/Graph.java
+++ server/src/main/java/analyzer/graphes/Graph.java
@@ -1,17 +1,20 @@
 package analyzer.graphes;
 
-import analyzer.reader.CodeReader;
 import analyzer.reader.CodeLine;
+import analyzer.reader.CodeReader;
 import analyzer.reader.Enums;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.List;
 import analyzer.graphes.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Graph
 {
 
     private String code;
+    private int paramertersCount = 0;
 
     public String getCode() {
         return code;
@@ -42,6 +45,8 @@ public class Graph
     {
         CodeReader reader = new CodeReader(this.code);
         CodeLine codeLine = reader.ReadLine();          // Function declaration
+
+        InitializeParametersNumber(codeLine);
 
         codeLine = reader.ReadLine();       // Start loop
 
@@ -77,6 +82,25 @@ public class Graph
         }
 
         return result;
+    }
+
+    private void InitializeParametersNumber(CodeLine line)
+    {
+        Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(line.getText());
+        List<ParamterItem> params = new ArrayList<>();
+        m.find();
+        String[] paramsString = m.group(1).split(",");
+        for (String s: paramsString)
+        {
+            String parameterName = GetParameterName(s);
+            params.add(new ParamterItem(parameterName, analyzer.graphes.Enums.Variables.Double, 0));
+        }
+    }
+
+    private String GetParameterName(String param)
+    {
+        String[] names = param.trim().split(" ");
+        return names[1];
     }
 
     @Override
